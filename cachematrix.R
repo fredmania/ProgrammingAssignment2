626f81b8a645e3a1bd17db0f423fae31849fd527
fredmania <fredrik.asmaro@gmail.com>
2014-10-25 12:50:12 +0200

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The code will calculate the inverse matrix. The inverse matrix will only be
+## calculated if it has not been calculated before
 
-## Write a short comment describing this function
+## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # Creates a NULL object
+        i <- NULL 
+        
+        # Sets the value of the matrix
+        setMatrix <- function(y) { 
+                x <<- y
+                i <<- NULL
+        }
+        
+        # Gets the value of the matrix
+        getMatrix <- function() x
+        
+        # Sets the value of the inverse matrix
+        setInverseMatrix <- function(inverse) i <<- inverse 
+        
+        # Gets the value of the inverse matrix
+        getInverseMatrix <- function() i 
+        
+        # Creates a list comprising matrix values and inverse matrix values
+        list(setMatrix = setMatrix, getMatrix = getMatrix, 
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special matrix returned by makeCacheMatrix, but only if it
+## has not been computed already
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        # Returns a matrix that is the inverse of x
+        i <- x$getInverseMatrix() 
+        
+        # Displays a message if the matrix is already stored in the cache, and then returns 
+        # the cached computation
+        if(!is.null(i)) {  
+                message("getting cached data")
+                return(i)
+        }
+        
+        # Calculates the matrix from the data
+        data <- x$getMatrix()
+        
+        # Stores calculation of solve funciton in i
+        i <- solve(data)
+        
+        # Returns the inverted matrix
+        x$setInverseMatrix(i)
+        i
 }
